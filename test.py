b2fe52efbc028ab112239111cf0efdac48b12e47
Le Wu <gaoxing0031@163.com>
2017-11-14 21:07:20 +0800

    repair some questions

--- test.py
+++ test.py
@@ -1,26 +1,26 @@
-import sys
-import caffe
+#coding=utf-8
 import numpy as np
-import os
+import sys,os
+import caffe
 
-dir = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)))
-model_file = os.path.join(dir, 'bvlc_googlenet_iter_175750.caffemodel')
-net_file = os.path.join(dir, 'deploy.prototxt')
-mean_file = os.path.join(dir, 'AVA1_mean.npy')
+
+net_file='deploy.prototxt'
+caffe_model='bvlc_googlenet_iter_175750.caffemodel'
+mean_file='AVA1_mean.npy'
 #if you have no GPUs,set mode cpu
-caffe.set_mode_gpu()
-net = caffe.Net(net_file, model_file, caffe.TEST)
-transformer = caffe.io.Transformer({'data':net.blobs['data'].data.shape})
-#set mean file may improve the result,but isn't must be
-#transformer.set_mean('data', np.load('../../python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))
-transformer.set_transpose('data',(2,0,1))
-transformer.set_mean('data', np.load(mean_file).mean(1).mean(1))  
-transformer.set_raw_scale('data',255)
-transformer.set_channel_swap('data',(2,1,0))
-net.blobs['data'].reshape(10,3,227,227)
-img = caffe.io.load_image('C:/Users/gaoxi/Desktop/4.jpg')
+caffe.set_mode_cpu()
+net = caffe.Net(net_file,caffe_model,caffe.TEST)
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_transpose('data', (2,0,1))
+transformer.set_mean('data', np.load(mean_file).mean(1).mean(1))
+transformer.set_raw_scale('data', 255) 
+transformer.set_channel_swap('data', (2,1,0))
+img = caffe.io.load_image('71.jpg')
 net.blobs['data'].data[...] = transformer.preprocess('data',img)
 out = net.forward()
 out1 = out["prob"][0]
-print "the score of the picture is:" + str(out1[0])
+print ("the score of the picture is:" + str(out1[0]))
+
+
+
+
