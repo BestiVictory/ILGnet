b09f28a184474247eb9a137caf8ca48083fb6303
Le Wu <gaoxing0031@163.com>
2017-11-14 22:01:37 +0800

    repair some questions

--- test.py
+++ test.py
@@ -15,11 +15,12 @@ transformer.set_transpose('data', (2,0,1))
 transformer.set_mean('data', np.load(mean_file).mean(1).mean(1))
 transformer.set_raw_scale('data', 255) 
 transformer.set_channel_swap('data', (2,1,0))
-img = caffe.io.load_image('71.jpg')
+img = caffe.io.load_image('good01.jpg')
 net.blobs['data'].data[...] = transformer.preprocess('data',img)
 out = net.forward()
-out1 = out["prob"][0]
-print ("the score of the picture is:" + str(out1[0]))
+out1 = out["loss1/loss"][0]
+print(out1)
+print ("the score of the picture is:" + str(out1[1]))
 
 
 
